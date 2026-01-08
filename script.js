e8607ed247bdff27ccde75100af6f3627ca17137
tpetrov06 <tpbusiness.etc2022@gmail.com>
2026-01-08 16:25:16 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+});
